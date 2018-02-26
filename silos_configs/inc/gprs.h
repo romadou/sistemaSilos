c913856fdcd5b89439d0a63a0783ce2a64043457
ivankras <ivankras@hotmail.com>
2018-02-26 16:02:17 -0300

    Eliminada sobreescritura de métodos en gprs

--- silos_configs/inc/gprs.h
+++ silos_configs/inc/gprs.h
@@ -6,10 +6,9 @@
 #endif
 
 void GPRS_config(void);
-void GPRS_config(int baud_rate);
-void GPRS_config(int baud_rate, char *dest_num);
 void GPRS_alerta(void);
 void GPRS_critico(void);
 void GPRS_set_dest_num(char *dest_num);
+char* GPRS_get_dest_num(void);
 
 #endif /* #ifndef GPRS_H_ */
--- silos_configs/src/gprs.c
+++ silos_configs/src/gprs.c
@@ -34,56 +34,6 @@ void GPRS_config(void){
    }
 }
 
-void GPRS_config(int baud_rate){
-   uint8_t dato = 0;   
-
-   /* Inicializar las UART según baud_rate */
-   uartConfig(UART_USB, baud_rate);
-   uartConfig(UART_232, baud_rate);
-   
-   //Activar impresión de códigos de error
-   uartWriteString( UART_232, "AT+CMEE=1\r\n");
-   delay(1000);
-   while( uartReadByte( UART_232, &dato ) ){
-      /* Se reenvía el dato a la UART_USB realizando un puente entre ambas */
-      uartWriteByte( UART_USB, dato );
-   }
-
-   //Activar servicio de mensajes
-   uartWriteString( UART_232, "AT+CMGF=1\r\n");
-   delay(1000);
-   while( uartReadByte( UART_232, &dato ) ){
-      /* Se reenvía el dato a la UART_USB realizando un puente entre ambas */
-      uartWriteByte( UART_USB, dato );
-   }
-}
-
-void GPRS_config(int baud_rate, char *dest_num){
-   uint8_t dato = 0;   
-
-   strcpy(dest_number, dest_num);
-
-   /* Inicializar las UART según baud_rate */
-   uartConfig(UART_USB, baud_rate);
-   uartConfig(UART_232, baud_rate);
-   
-   //Activar impresión de códigos de error
-   uartWriteString( UART_232, "AT+CMEE=1\r\n");
-   delay(1000);
-   while( uartReadByte( UART_232, &dato ) ){
-      /* Se reenvía el dato a la UART_USB realizando un puente entre ambas */
-      uartWriteByte( UART_USB, dato );
-   }
-
-   //Activar servicio de mensajes
-   uartWriteString( UART_232, "AT+CMGF=1\r\n");
-   delay(1000);
-   while( uartReadByte( UART_232, &dato ) ){
-      /* Se reenvía el dato a la UART_USB realizando un puente entre ambas */
-      uartWriteByte( UART_USB, dato );
-   }
-}
-
 void GPRS_alerta(void){
    uint8_t i;
    uint8_t dato  = 0;
@@ -132,4 +82,10 @@ void GPRS_critico(void){
 
 void GPRS_set_dest_num(char *dest_num){
    strcpy(dest_number, dest_num);
+}
+
+char* GPRS_get_dest_num(void){
+   char dest_aux[10];
+   strcpy(dest_aux, dest_number);
+   return dest_aux;
 }
