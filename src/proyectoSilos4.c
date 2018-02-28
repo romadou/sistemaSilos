f496ffe747fae9207b1cffb1cae7dba466f4e2e5
delfina <delfinarambarri@gmail.com>
2018-02-28 17:45:29 -0300

    Promedio probado con 0, 1, 4, 5 y 32 esclavos

--- src/proyectoSilos4.c
+++ src/proyectoSilos4.c
@@ -16,8 +16,9 @@
 #include "lcd.h"
 #include "uart.h"
 #include "lcd_mensajes.h"
-#include "packageGenerator.h"
-
+#include "testTabla.h"
+#include "math.h"
+#include "general_config.h"
 
 
 /*==================[definiciones y macros]==================================*/
@@ -37,6 +38,7 @@ uint8_t buffer[8];
 uint8_t posBuffer;
 uint8_t indice;
 uint8_t dato;
+
 
 /*==================[definiciones de datos externos]=========================*/
 
@@ -60,7 +62,6 @@ int main( void )
    boardConfig();
    uartConfig(0,9600);
    uartEnableRxInterrupt(4);
-   //uartEnableRxInterrupt(4);
    // ---------- INICIAR SISTEMA OPERATIVO --------------------
 	// Starts the operating system in the Application Mode 1
 	// This example has only one Application Mode
@@ -87,21 +88,13 @@ TASK (InitTask){
 
 	key='s';
 	inactiveSlaves=0;
-	//HASTA OBTENER VALORES DE LOS ESCLAVOS
-	temperatureI=23;
-	humidityI=70;
-	temperatureE=21;
-	humidityE=65;
-	desiredTemp=22;
-
-	//HASTA OBTENER VALORES DE LOS ESCLAVOS
-	fan=0;
 	VENTILACION_Init();
 	KEYPAD_init();
 	LCD_init(DISPLAY_8X5 | _2_LINES , DISPLAY_ON | CURSOR_OFF);
 	MEF_Init();
+	CONFIG_init();
 	SetRelAlarm(ActivateKeypadTask, 1000, 100);
-	SetRelAlarm(ActivateRefreshDisplayTask, 1000, 600);
+	SetRelAlarm(ActivateRefreshDisplayTask, 1000, 500);
 	SetRelAlarm(ActivateDetectActiveSlavesTask, 1000, 6000);
 	TerminateTask();
 }
@@ -136,7 +129,8 @@ TASK(TakeDataSlavesTask){
 }
 TASK(ActionTask){
 	gpioToggle( LEDB );	
-	if(VENTILACION_actuar(temperatureI, temperatureE, humidityI, humidityE, desiredTemp))
+	processData();
+	if(VENTILACION_actuar(temperatureI, temperatureE, humidityI, humidityE, CONFIG_get_desired_temp()))
 		fan=1;
 	else
 		fan=0;
@@ -145,28 +139,58 @@ TASK(ActionTask){
 }
 //rutina de interrupción utilizada para la primer prueba de recepción de byte
 ISR (UART_RS485_IRQHandler){
+	/**
 	uartReceiveByte(4, &dato);
 	saveInBuffer(dato);
-
+	**/
 }
 /*==================[definiciones de funciones internas]=====================*/
 
 
 void processData (void){
-	uint8_t i, totalHum=0, totalTemp=0, cantTotal=0;
-	for(i=1; i<31; i++){ //el esclavo 0 está en el exterior, por eso no se incluye en el promedio.
-		if(slaves[i][C_STATE]==1){ //está activo
+	uint8_t i;
+	float tempAux, humAux;
+	unsigned short totalHum=0, totalTemp=0;
+	activeSlaves=0;
+	for(i=1; i<MAXSLAVES; i++){ //el esclavo 0 está en el exterior, por eso no se incluye en el promedio.
+		if(slaves[i][C_STATE]==ACTIVE){ //está activo
+			activeSlaves++;
 			totalTemp=totalTemp + slaves[i][C_TEMP];
 			totalHum=totalHum + slaves[i][C_HUM];
-			cantTotal++;
 		}
-		if(slaves[i][C_INACTIVE]>=30){
+		if(slaves[i][C_INACTIVE]>=MAX_INACTIVE)
 			inactiveSlaves=1; //en principio solo se notifica que hay al menos 1 inactivo, lo mejor sería indicar cual/es
-		}
-
-	humidityI=totalHum/cantTotal;
-	temperatureI=totalTemp/cantTotal;	
+		
 	}
+	if(activeSlaves>0){ //si no hay esclavos activos no tiene sentido hacer el promedio
+		tempAux=(float)totalTemp/activeSlaves;
+		if(tempAux-(uint8_t)tempAux > 0.5) //se redondea
+			temperatureI=ceil(tempAux);
+		else
+			temperatureI=floor(tempAux);
+		
+		humAux=(float)totalHum/activeSlaves; 
+		if(humAux-(uint8_t)humAux > 0.5) //se redondea
+			humidityI=ceil(humAux);
+		else
+			humidityI=floor(humAux);
+	}
+	else{ //si no hay datos
+		temperatureI=255; //momentaneo. Lo ideal sería mostrar en pantalla 'XX'
+		humidityI=255;
+	}
+	if(slaves[0][0]==ACTIVE){ //Esclavo exterior
+	activeSlaves++;
+	temperatureE=slaves[0][2];
+	humidityE=slaves[0][3];
+	}
+	else{ 
+		/*Si el sensor exterior no funciona el sistema no funciona
+		Debe agregarse a estado de alerta!!*/
+		temperatureE=255; //momentaneo. Lo ideal sería mostrar en pantalla 'XX'
+		humidityE=255;		
+	}
+
 
 }
 
